fee5ead4d59b927056a3d35e4a82c1c02ea0cb01
aldngrha <aldi.nugrahatk@gmai.com>
2020-12-21 11:11:57 +0700

    upload project vue

---
+++ src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+    <HeaderShayna />
+    <Hero />
+    <WomenShayna />
+    <Insta />
+    <Partner />
+    <Footer />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HeaderShayna from "@/components/HeaderShayna.vue";
+import Hero from "@/components/Hero.vue";
+import WomenShayna from "@/components/WomenShayna.vue";
+import Insta from "@/components/Insta.vue";
+import Partner from "@/components/Partner.vue";
+import Footer from "@/components/Footer.vue";
+export default {
+  name: "home",
+  components: {
+    HeaderShayna,
+    Hero,
+    WomenShayna,
+    Insta,
+    Partner,
+    Footer,
+  },
+};
+</script>
